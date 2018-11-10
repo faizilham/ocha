99a25d5826aafee0d3eef263f192148e6cb92edb
Faiz Ilham <faizilham.faizz@gmail.com>
2018-11-10 17:06:23 +0700

    remove unnecessary type

--- src/helper.rs
+++ src/helper.rs
@@ -1,11 +1,5 @@
-use std::cell::{Cell, RefCell};
+use std::cell::RefCell;
 use std::rc::Rc;
-
-pub type PCell<T> = Rc<Cell<T>>;
-
-pub fn new_pcell<T> (t : T) -> PCell<T> {
-    Rc::new(Cell::new(t))
-}
 
 pub type PRefCell<T> = Rc<RefCell<T>>;
 
